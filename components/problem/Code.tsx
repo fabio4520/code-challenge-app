55369b06d8e0443f591558de74845ff0b26726b0
Fabio Fiestas <fabioleofc@gmail.com>
2024-02-13 09:02:31 -0500

    code section

--- components/problem/Code.tsx
+++ components/problem/Code.tsx
@@ -3,7 +3,12 @@ import problemStyle from "./Problem.module.css"
 export default function Code() {
   return (
     <div className={problemStyle.card}>
-      Code
+      <div className={problemStyle["card-header"]}>
+        <p className='font-bold'>&lt;/&gt; Code</p>
+      </div>
+      <div className="p-6">
+      <textarea className="w-full dark:bg-gray-900 p-6" rows={16} autoFocus>def solution(arr):</textarea>
+      </div>
     </div>
   )
 }
